183b21e169597403726bc2f50a458bebaa270ca8
Thomaz <thomazbarbosa1244@gmail.com>
2021-09-03 00:29:04 -0300

    Testing connection with args

--- src/main/java/com/thomaz/port/PortfolioApplication.java
+++ src/main/java/com/thomaz/port/PortfolioApplication.java
@@ -2,12 +2,23 @@ package com.thomaz.port;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class PortfolioApplication {
+@ComponentScan
+public class PortfolioApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PortfolioApplication.class);
+    }
+
+
 
 }
