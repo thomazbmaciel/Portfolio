f047495ba71ac1b819624cd1be86d0b56f1e3eeb
Thomaz <thomazbarbosa1244@gmail.com>
2021-09-03 00:37:09 -0300

    Reverting some changes

--- src/main/java/com/thomaz/port/PortfolioApplication.java
+++ src/main/java/com/thomaz/port/PortfolioApplication.java
@@ -2,23 +2,12 @@ package com.thomaz.port;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
-public class PortfolioApplication extends SpringBootServletInitializer{
+public class PortfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
-
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(PortfolioApplication.class);
-    }
-
-
 
 }
